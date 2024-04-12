a4ee0fbf07616c46f10ab666ca8b7023f59c6df1
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2024-04-12 19:14:11 -0300

    fix: arquivos estáticos não apareciam em produção

--- src/urls.py
+++ src/urls.py
@@ -35,6 +35,6 @@ if settings.DEBUG:
     urlpatterns.append(path(f"{settings.ROOT_URL_PATH}/__debug__/", include(debug_toolbar.urls)))
 else:
     urlpatterns += [
-        re_path(f"{settings.STATIC_URL}/media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
-        re_path(f"{settings.STATIC_URL}/static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+        re_path(f"{settings.ROOT_URL_PATH}/media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+        re_path(f"{settings.ROOT_URL_PATH}/static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
     ]
